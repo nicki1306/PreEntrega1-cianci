0d15677147f2e0f36b787c45344523983862fbb0
nicki1306 <nickii.cn@hotmail.com>
2023-10-31 20:37:30 -0300

    proyecto final

--- main.js
+++ main.js
@@ -1,27 +1,37 @@
 function comprar(){
 
-    let identificar = true
-    let cantidad = 2
-    let producto = gorras
+    let nombre = ""
+    do{
+        nombre = prompt("ingrese usuario")
+    }while (nombre=="")
+    
+    alert("bienvenido " + nombre)
+
+    let producto = "gorras"
+    let precio = 15000
+    alert("Elije tus productos (maximo 3 unidades)" + producto)
+    let cantidad = 0
+    do{
+        cantidad= prompt("agregue cantidad de unidades de " + producto)
+        console.log("la cantidad de gorras es " + cantidad)
+        if (cantidad>=3) {
+            alert("supera el limite maximo para este producto")
+            
+        }
+        
+    }while((cantidad>=3)|| (cantidad== ""))
+console.log(cantidad)
+    if(cantidad>=1){
+        let total = 0
+        total = precio * cantidad
+        alert("el total a pagar es " + total)
+        let pagar = ""
+        do {
+            pagar = prompt("seleccione medio de pago (visa, master o transferencia")
+        } while (pagar=="");
+        alert("pago existoso")
+    }
 
 }
 
-    do{
-        let cantidad = prompt("usted puede agregar hasta 2 unidades de " + producto)
-        if(cantidad === null){
-            break
-        }
-        if(cantidad== "2"){
-            alert("productos ingresados con exito")
-            identificar = false
-            }else{
-                alert("falta una unidad para completar el carrito")
-                if(cantidad >2){
-                    alert("Su carrito supera el limite de compra de este producto, elimine uno")
-                    console.log("intente nuevamente")
-                    break
-            }
-        }
-    }while(identificar)
-
-    comprar()
+comprar()
