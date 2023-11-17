77172e1e830047b494a1ef1065c86e45789cde2e
nicki1306 <nickii.cn@hotmail.com>
2023-11-17 18:07:39 -0300

    continuo entrega

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ function comprar(){
     
     alert("bienvenido " + nombre)
 
-    let producto = "gorras"
+    let producto = "zapatillas"
     let precio = 15000
     alert("Elije tus productos (maximo 3 unidades)" + producto)
     let cantidad = 0
     do{
         cantidad= prompt("agregue cantidad de unidades de " + producto)
-        console.log("la cantidad de gorras es " + cantidad)
+        console.log("la cantidad de zapatillas es " + cantidad)
         if (cantidad>=3) {
             alert("supera el limite maximo para este producto")
             
@@ -35,3 +35,23 @@ console.log(cantidad)
 }
 
 comprar()
+
+const Zapatillas = function (color,talles){
+    this.color = color
+    this.talles= talles
+
+}
+
+
+let zapatillas1 = new zapatillas1 ("rojo, 35,36,37,38,39,40")
+let zapatillas2 = new zapatillas2 ("azul, 35,36,37,38,39,40")
+let zapatillas3 = new zapatillas3 ("gris, 35,36,37,38,39,40")
+let zapatillas4 = new zapatillas4 ("negro, 35,36,37,38,39,40")
+let zapatillas5 = new zapatillas5 ("blanco, 35,36,37,38,39,40")
+let zapatillas6 = new zapatillas6 ("amarillo, 35,36,37,38,39,40")
+
+let lista [zapatillas1,zapatillas2,zapatillas3,zapatillas4,zapatillas5,zapatillas6]
+
+function buscarzapatillas(){
+    
+}
